c5a6b83efa9cdacd84ae5f95241ed9ddfaf59c02
vickyleonardo23 <vickyleonardo23@gmail.com>
2025-06-19 09:42:45 +0000

    update

--- resources/views/backend/department/show.blade.php
+++ resources/views/backend/department/show.blade.php
@@ -24,6 +24,21 @@
                         </svg>
                     </button>
                 </div>
+            @elseif (session('error'))
+                <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-200 dark:bg-gray-800 dark:text-red-400 relative"
+                    role="alert">
+                    <span class="font-medium">{{ session('error') }}!</span>
+                    <!-- Tombol silang dengan SVG -->
+                    <button type="button"
+                        class="absolute top-0 right-0 p-4 rounded-md text-red-600 hover:bg-red-300 hover:text-red-800"
+                        aria-label="Close" onclick="this.parentElement.style.display='none';">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                </div>
             @endif
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-10 flex flex-col gap-y-5">
                 <div class="item-card flex flex-row gap-y-10 justify-between items-center">
